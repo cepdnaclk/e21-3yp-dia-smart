a2224c102f342893e71f2260bbdb21669d338cce
Arnikan18 <e21036@eng.pdn.ac.lk>
2026-05-01 22:42:13 +0530

    Revert "Merge pull request #8 from cepdnaclk/develop"
    This reverts commit 17061133d4617e234c0e6e9928da67071c34fb0f, reversing
    changes made to 08a2f0bf3e5bbd8c1e0d5aeb182cdc0c2649d35d.

--- frontend/mobile-app/rn-app/app/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useAuth } from "@/state/authContext";
-import { Redirect } from "expo-router";
-
-export default function IndexScreen() {
-  const { user } = useAuth();
-  if (user) return <Redirect href="/(tabs)/home" />;
-  return <Redirect href="/login" />;
-}

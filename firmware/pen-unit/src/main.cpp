b1a1735b3a072d6bb37ec981126e3097944229e0
ananthu65 <e21031@eng.pdn.ac.lk>
2026-06-05 23:08:06 +0530

    Revert "feat(firmware/pen-unit): save doses before queueing"
    This reverts commit 3739b37256d8b22e64697e29f7713c7f86c464ed.

--- firmware/pen-unit/src/main.cpp
+++ firmware/pen-unit/src/main.cpp
@@ -3,15 +3,6 @@
 #include <freertos/queue.h>
 #include "config/app_config.h"
 #include "models/dose_event.h"
-#include "services/storage_service.h"
-
-#if CONFIG_FREERTOS_UNICORE
-static constexpr BaseType_t DOSE_TASK_CORE = 0;
-static constexpr BaseType_t BLE_TASK_CORE = 0;
-#else
-static constexpr BaseType_t DOSE_TASK_CORE = 1;
-static constexpr BaseType_t BLE_TASK_CORE = 0;
-#endif
 
 // Forward declarations for tasks defined in tasks/
 void doseDetectionTask(void* pvParams);
@@ -19,20 +10,11 @@ void bleTransferTask(void* pvParams);
 
 // Shared queue: doseDetectionTask → bleTransferTask
 QueueHandle_t doseEventQueue = nullptr;
-PenDoseStorageService doseStorage;
 
 void setup() {
     Serial.begin(SERIAL_BAUD);
     delay(200);
     Serial.println("=== Dia-Smart Pen Unit Starting ===");
-
-    if (!doseStorage.begin()) {
-        Serial.println("[FATAL] Failed to initialise dose storage - halting");
-        while (true) { delay(1000); }
-    }
-    Serial.printf("[Main] Dose storage ready: %u pending / %u capacity\n",
-                  doseStorage.countByStatus(DOSE_RECORD_PENDING),
-                  doseStorage.capacity());
 
     // Create shared queue before starting tasks
     doseEventQueue = xQueueCreate(DOSE_QUEUE_LENGTH, sizeof(DoseEvent));
@@ -49,7 +31,7 @@ void setup() {
         nullptr,
         2,          // higher priority so button presses are never missed
         nullptr,
-        DOSE_TASK_CORE
+        1           // Core 1
     );
 
     // BLE GATT server on Core 0 (Bluetooth stack lives on Core 0)
@@ -60,7 +42,7 @@ void setup() {
         nullptr,
         1,
         nullptr,
-        BLE_TASK_CORE
+        0           // Core 0
     );
 
     Serial.println("[Main] FreeRTOS tasks started");
